271d2a257d06eac0411ae3a793e5299fc07c89e8
Florin Andrei <florin@andrei.myip.org>
2019-08-13 13:53:57 -0700

    main script, initial commit

---
+++ soundspec.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+
+from scipy.io import wavfile
+from scipy import signal
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+
+# common sense limits for frequency
+fmin = 10
+fmax = 20000
+
+if len(sys.argv) != 2:
+  print('Usage:', sys.argv[0], 'file.wav')
+  sys.exit(1)
+
+print('Reading the audio file...')
+sf, audio = wavfile.read(sys.argv[1])
+
+if sf < fmin:
+  print('Sampling frequency too low.')
+  sys.exit(1)
+
+# convert to mono
+sig = np.mean(audio, axis=1)
+# number of points per segment; more points = better frequency resolution
+# if equal to sf, then frequency resolution is 1 Hz
+npts = int(sf)
+
+print('Calculating FFT...')
+f, t, Sxx = signal.spectrogram(sig, sf, nperseg=npts)
+
+# FFT at high resolution makes way too many frequencies
+# set some lower number of frequencies we would like to keep
+# final result will be even smaller after pruning
+nf = 1000
+# generate an exponential distribution of frequencies
+# (as opposed to the linear distribution from FFT)
+b = fmin - 1
+a = np.log10(fmax - fmin + 1) / (nf - 1)
+freqs = np.empty(nf, int)
+for i in range(nf):
+  freqs[i] = np.power(10, a * i) + b
+# list of frequencies, exponentially distributed:
+freqs = np.unique(freqs)
+
+# delete frequencies lower than fmin
+fnew = f[f >= fmin]
+cropsize = f.size - fnew.size
+f = fnew
+Sxx = np.delete(Sxx, np.s_[0:cropsize], axis=0)
+
+# delete frequencies higher than fmax
+fnew = f[f <= fmax]
+cropsize = f.size - fnew.size
+f = fnew
+Sxx = Sxx[:-cropsize, :]
+
+findex = []
+# find FFT frequencies closest to calculated exponential frequency distribution
+for i in range(freqs.size):
+  f_ind = (np.abs(f - freqs[i])).argmin()
+  findex.append(f_ind)
+
+# keep only frequencies closest to exponential distribution
+# this is usually a massive cropping of the initial FFT data
+fnew = []
+for i in findex:
+  fnew.append(f[i])
+f = np.asarray(fnew)
+Sxxnew = Sxx[findex, :]
+Sxx = Sxxnew
+
+print('Generating the image...')
+plt.pcolormesh(t, f, np.log10(Sxx))
+plt.ylabel('f [Hz]')
+plt.xlabel('t [sec]')
+plt.yscale('symlog')
+plt.ylim(fmin, fmax)
+
+# TODO: make this depend on fmin / fmax
+yt = np.arange(10, 100, 10)
+yt = np.concatenate((yt, 10 * yt, 100 * yt, 1000 * yt))
+yt = yt[yt <= fmax]
+yt = yt.tolist()
+plt.yticks(yt)
+
+plt.grid(True)
+plt.show()
